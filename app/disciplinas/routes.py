9c74ae66e50ff35000a63edb2aee95b5cc56d3aa
Manoel Guilherme <fisicorj@gmail.com>
2025-05-18 16:54:32 -0300

    .

--- app/disciplinas/routes.py
+++ app/disciplinas/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from app import db
-from app.models import Disciplina
+from app.models import Disciplina, Turma, Professor
 
 disciplinas_bp = Blueprint('disciplinas', __name__, url_prefix='/disciplinas')
 
@@ -17,13 +17,17 @@ def nova():
     if request.method == 'POST':
         d = Disciplina(
             nome=request.form['nome'],
-            sigla=request.form['sigla']
+            sigla=request.form['sigla'],
+            turma_id=request.form['turma_id'],
+            professor_id=request.form['professor_id']
         )
         db.session.add(d)
         db.session.commit()
         flash('Disciplina cadastrada com sucesso.', 'success')
         return redirect(url_for('disciplinas.listar'))
-    return render_template('disciplinas/form.html', titulo='Nova Disciplina')
+    turmas = Turma.query.all()
+    professores = Professor.query.all()
+    return render_template('disciplinas/form.html', titulo='Nova Disciplina', turmas=turmas, professores=professores)
 
 @disciplinas_bp.route('/editar/<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -32,10 +36,14 @@ def editar(id):
     if request.method == 'POST':
         d.nome = request.form['nome']
         d.sigla = request.form['sigla']
+        d.turma_id = request.form['turma_id']
+        d.professor_id = request.form['professor_id']
         db.session.commit()
         flash('Disciplina atualizada.', 'info')
         return redirect(url_for('disciplinas.listar'))
-    return render_template('disciplinas/form.html', disciplina=d, titulo='Editar Disciplina')
+    turmas = Turma.query.all()
+    professores = Professor.query.all()
+    return render_template('disciplinas/form.html', disciplina=d, titulo='Editar Disciplina', turmas=turmas, professores=professores)
 
 @disciplinas_bp.route('/excluir/<int:id>')
 @login_required
